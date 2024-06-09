9ee97718b47685c0ef5fdd5c2bb8b4337ed48469
du-cki <qwuakers@gmail.com>
2024-06-09 19:58:01 +0400

    test

--- src/routes/pineapple.rs
+++ src/routes/pineapple.rs
@@ -1,81 +1,101 @@
 use axum::{
     extract::{
-        ws::{WebSocket, WebSocketUpgrade, Message},
-        ConnectInfo, State
+        ws::{Message, WebSocket, WebSocketUpgrade},
+        ConnectInfo, State,
     },
-    response::IntoResponse
+    response::IntoResponse,
 };
-use futures::{StreamExt, stream::{SplitStream, SplitSink}, SinkExt};
-use tokio::sync::{broadcast::Receiver, Mutex};
+use futures::StreamExt;
+use tokio::{
+    sync::{broadcast::Receiver, Mutex},
+    time::{self, Duration},
+};
 
 use std::{net::SocketAddr, sync::Arc};
 
 use crate::{
-    models::{AppState, File, Folder, GenericRequest, FileRecord, Events},
-    json_message
+    json_message,
+    models::{AppState, Events, File, FileRecord, Folder, GenericRequest},
 };
 
-struct FileUpdates {
-    receiver: Arc<Mutex<SplitStream<WebSocket>>>,
-    sender:   Arc<Mutex<SplitSink<WebSocket, Message>>>,
-    rx:       Arc<Mutex<Receiver<FileRecord>>>,
-    state:    AppState
+struct WSHandler {
+    socket: WebSocket,
+    rx: Receiver<FileRecord>,
+    state: AppState,
 }
 
-impl FileUpdates {
-    async fn from(
-        sock: WebSocket, rx: Receiver<FileRecord>, state: AppState
-    ) {
-        let (sender, receiver) = sock.split();
-
-        let s = Self {
-            receiver: Arc::new(Mutex::new(receiver)),
-            sender:   Arc::new(Mutex::new(sender)),
-            rx:       Arc::new(Mutex::new(rx)),
-            state
-        };
-
-        s.respond_to_messages().await;
-        // tokio::join!(
-        //     tokio::spawn(s.file_upload_events())
-        // );
+impl WSHandler {
+    async fn from(socket: WebSocket, rx: Receiver<FileRecord>, state: AppState) {
+        Self { socket, rx, state }.handle().await;
     }
 
-    async fn file_upload_events(&self) {
-        while let Ok(file) = self.rx.lock().await.recv().await {
-            let mut writer = self.sender.lock().await;
-
-            let _ = writer.send(json_message!(
-                "event" => Events::FileUpload.to_string(),
-                "file" => file
-            )).await;
-        }
-    }
-
-    async fn respond_to_messages(&self) {
-        while let Some(Ok(message)) = self.receiver.lock().await.next().await {
-            if let Ok(msg) = message.to_text() {
-                if let Ok(data) = serde_json::from_str::<GenericRequest>(msg) {
-                    let msg: Message = match data.event {
-                        Events::QueryFolder => self.query_folder(data).await,
-                        other => {
-                            tracing::error!("unimplemented event `{:?}` received, ignoring...", other);
-                            continue;
-                        },
-                    };
-
-                    let mut sender = self.sender.lock().await;
-                    let _ = sender.send(msg).await;
-                }
+    async fn handle(&mut self) {
+        loop {
+            if let Ok(file) = self.rx.recv().await {
+                let _ = self
+                    .socket
+                    .send(json_message!(
+                        "event" => Events::FileUpload.to_string(),
+                        "file" => file
+                    ))
+                    .await;
             }
+
+            let message = self.socket.next().await;
+
+            if let Some(Ok(message)) = message {
+                if let Ok(msg) = message.to_text() {
+                    if let Ok(data) = serde_json::from_str::<GenericRequest>(msg) {
+                        let msg: Message = match data.event {
+                            Events::QueryFolder => self.query_folder(data).await,
+                            other => {
+                                tracing::error!(
+                                    "unimplemented event `{:?}` received, ignoring...",
+                                    other
+                                );
+                                continue;
+                            }
+                        };
+
+                        let _ = self.socket.send(msg).await;
+                    }
+                }
+            } else if let None = message {
+                tracing::info!("client disconnected, exiting loop...");
+                break;
+            }
+
+            time::sleep(Duration::from_secs(1)).await;
+            tracing::info!("Done iteration, sleeping...")
         }
     }
 
-    async fn query_folder(
-        &self,
-        request: GenericRequest,
-    ) -> Message {
-        let folder_query = sqlx::query_as!(Folder, r#"
+    // async fn respond_to_messages(&self) {
+    //     while let Some(Ok(message)) = self.receiver.lock().await.next().await {
+    //         if let Ok(msg) = message.to_text() {
+    //             if let Ok(data) = serde_json::from_str::<GenericRequest>(msg) {
+    //                 let msg: Message = match data.event {
+    //                     Events::QueryFolder => self.query_folder(data).await,
+    //                     other => {
+    //                         tracing::error!(
+    //                             "unimplemented event `{:?}` received, ignoring...",
+    //                             other
+    //                         );
+    //                         continue;
+    //                     }
+    //                 };
+
+    //                 let mut sender = self.sender.lock().await;
+    //                 let _ = sender.send(msg).await;
+    //             }
+    //         }
+    //     }
+    // }
+
+    async fn query_folder(&self, request: GenericRequest) -> Message {
+        let folder_query = sqlx::query_as!(
+            Folder,
+            r#"
             SELECT
                 id, path as "path!: String"
             FROM folder_paths
@@ -83,10 +103,14 @@ impl FileUpdates {
         "#,
             request.data
         )
-            .fetch_optional(&*self.state.pool).await.unwrap();
+        .fetch_optional(&*self.state.pool)
+        .await
+        .unwrap();
 
         if let Some(folder) = folder_query {
-            let file_query = sqlx::query_as!(File, r#"
+            let file_query = sqlx::query_as!(
+                File,
+                r#"
                 SELECT file_id as "id!",
                     file_name || COALESCE('.' || file_ext, '') as "name!",
                     1 AS "file_type!",
@@ -108,15 +132,15 @@ impl FileUpdates {
             "#,
                 folder.id
             )
-                .fetch_all(&*self.state.pool)
-                .await;
+            .fetch_all(&*self.state.pool)
+            .await;
 
             if let Ok(files) = file_query {
                 return json_message!(
                     "event" => Events::QueryFolder,
                     "files" => files,
                     "request_id" => request.request_id
-                )
+                );
             }
         }
 
@@ -128,30 +152,18 @@ impl FileUpdates {
     }
 }
 
-
-
 pub async fn route(
     ws: WebSocketUpgrade,
     State(state): State<AppState>,
     ConnectInfo(addr): ConnectInfo<SocketAddr>,
 ) -> impl IntoResponse {
-    ws.on_upgrade(move |socket| pineapple(
-        socket,
-        state,
-        addr,
-    ))
+    ws.on_upgrade(move |socket| pineapple(socket, state, addr))
 }
-
 
 async fn pineapple(
     socket: WebSocket,
     state: AppState,
     _: SocketAddr, // TODO
 ) {
-    FileUpdates::from(
-        socket,
-        state.sx.subscribe(),
-        state
-    ).await
+    WSHandler::from(socket, state.sx.subscribe(), state).await
 }
-
--- src/routes/upload_file.rs
+++ src/routes/upload_file.rs
@@ -1,21 +1,20 @@
 use axum::{
     extract::{Multipart, Query, State},
     http::StatusCode,
-    Json
+    Json,
 };
 use hex::encode;
 use nanoid::nanoid;
 
 use sha2::{Digest, Sha256};
 
-use tokio::{fs::{File, self}, io::AsyncWriteExt};
+use tokio::{fs::File, io::AsyncWriteExt};
 
 use super::AppState;
 use crate::{
     models::{APIError, FileRecord, UploadQuery, UploadResponse},
-    utils::{internal_error, FileInfo}
+    utils::{internal_error, FileInfo},
 };
-
 
 pub async fn route(
     State(state): State<AppState>,
@@ -23,9 +22,7 @@ pub async fn route(
     mut multipart: Multipart,
 ) -> Result<(StatusCode, Json<UploadResponse>), (StatusCode, Json<APIError>)> {
     while let Ok(Some(field)) = multipart.next_field().await {
-        let original_file_name = field.file_name()
-            .unwrap_or_else(|| "unknown")
-            .to_string();
+        let original_file_name = field.file_name().unwrap_or_else(|| "unknown").to_string();
 
         let file_ext = FileInfo::from_str(&original_file_name).ext;
 
@@ -34,9 +31,7 @@ pub async fn route(
             .unwrap_or_else(|| &state.config.fallback_content_type)
             .to_string();
 
-        let data = field.bytes()
-            .await
-            .map_err(internal_error)?;
+        let data = field.bytes().await.map_err(internal_error)?;
 
         let file_hash = {
             let mut hash = Sha256::new();
@@ -47,12 +42,13 @@ pub async fn route(
 
         let len = state.config.file_name_length;
 
-        let file_name = state.config.retain_uploaded_file_name
+        let file_name = state
+            .config
+            .retain_uploaded_file_name
             .then_some(original_file_name.clone())
             .or(nanoid!(len).into());
 
-        let is_public = query_param.public
-            .unwrap_or(state.config.default_public) as u16;
+        let is_public = query_param.public.unwrap_or(state.config.default_public) as u16;
 
         let fp = {
             let file_exists = sqlx::query!(
@@ -62,9 +58,9 @@ pub async fn route(
             "#,
                 file_hash
             )
-                .fetch_optional(&*state.pool)
-                .await
-                .map_err(internal_error)?;
+            .fetch_optional(&*state.pool)
+            .await
+            .map_err(internal_error)?;
 
             if let Some(record) = file_exists {
                 record.file_path
@@ -105,21 +101,28 @@ pub async fn route(
                     SELECT path FROM folder_paths WHERE id = media.folder_id
                  ) || '/' || media.file_name || COALESCE("." || media.file_ext, "") AS "file_url!"
             "#,
-            is_public, file_name, fp,
-            content_type, file_hash, file_ext,
-            original_file_name, query_param.folder
+            is_public,
+            file_name,
+            fp,
+            content_type,
+            file_hash,
+            file_ext,
+            original_file_name,
+            query_param.folder
         )
         .fetch_one(&*state.pool)
         .await
-        .map_err(|_|  {
+        .map_err(|_| {
             // TODO: fs::remove_file(fp).await else;
 
             // should be the only error that could possible be raised by this query
             // not sure how I can filter the error and get the proper error anyway.
-            (StatusCode::NOT_FOUND,
-             Json(APIError {
-                 message: format!("folder `{}` not found", query_param.folder.clone().unwrap())
-             }))
+            (
+                StatusCode::NOT_FOUND,
+                Json(APIError {
+                    message: format!("folder `{}` not found", query_param.folder.clone().unwrap()),
+                }),
+            )
         })?;
 
         if state.sx.receiver_count() > 0 {
